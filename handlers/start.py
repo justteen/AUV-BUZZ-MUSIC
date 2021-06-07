6ee63bce13b32eb6553f22482ced28f2b4fc21a1
justteen <48372964+justteen@users.noreply.github.com>
2021-06-07 08:01:15 +0700

    AUV

---
+++ handlers/start.py
@@ -0,0 +1,61 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import BOT_NAME as bn
+
+
+
+
+@Client.on_message(filters.command("start") & filters.private & ~filters.channel)
+async def start(_, message: Message):
+    await message.reply_text(
+        f"""**HELLO!, Saya  {bn}, Adalah bot untuk memutar musik dalam obrolan suara gruop chat anda.\n\n Jangan lupa untuk menambahkan asisten musik juga, agar dapat memutar musiknya.\n\n /help untuk mengetahui perintah**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Support⚡️", url="https://t.me/ossuport3"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "Creator⚡️", url="https://t.me/psycho_syridwan"
+                    ),
+                    InlineKeyboardButton(
+                        "Group⚡️", url="https://t.me/ossuport"
+                    )
+                    
+                ],
+                [
+                    InlineKeyboardButton(
+                        "Tambahkan bot ke Group⚡️", url="http://t.me/auvbuzzbot?startgroup=true"
+                    )
+                    
+                ],
+                [
+                    InlineKeyboardButton(
+                        "Tambahkan Asiten⚡️", url="http://t.me/@asistenmusik2?startgroup=True"
+                    )
+                ]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
+
+@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
+async def gstart(_, message: Message):
+      await message.reply_text(
+        "💁🏻‍♂️ apakah kamu ingin mencari lewat YouTube video?",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "✅ Yes", switch_inline_query_current_chat=""
+                    ),
+                    InlineKeyboardButton(
+                        "No ❌", callback_data="close"
+                    )
+                ]
+            ]
+        )
+    )
