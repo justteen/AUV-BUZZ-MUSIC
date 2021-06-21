019b02949d0b0da251c51290f87e5a565e2b268a
justteen <48372964+justteen@users.noreply.github.com>
2021-06-21 16:18:52 +0700

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,5 +5,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Hello! Saya asisten music .\n\n ❗️ Rules:\n   - TIDAK SPAM CHAT \n\n 👉 **Kirim link group apabila asisten tidak dapat masuk.**\n\n ⚠️ PENGINGAT: JANGAN MENGIRIM PESAN PENTING DISINI\n    - JANGAN TAMBAHKAN KE GROUP PRIVASI ANDA.\n   - JANGAN BAGI INFORMASI PENTING DISINI\n\n")
+  await USER.send_message(message.chat.id,"Hello! Saya asisten music .\n\n ❗️ Rules:\n   - TIDAK SPAM CHAT \n\n 👉 **Kirim link group apabila asisten tidak dapat masuk.**\n\n ⚠️ PENGINGAT: JANGAN MENGIRIM PESAN PENTING DISINI\n    - JANGAN TAMBAHKAN KE GROUP PRIVASI ANDA.\n   - JANGAN BAGI INFORMASI PENTING DISINI\n\nFOR SUPPORT @Kabaridevbot_bot")
   return                        
